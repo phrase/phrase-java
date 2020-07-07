c78cdabaac92f35a3ddcbed666b824dcbc06d3ba
Phrase <support@phrase.com>
2020-07-07 12:13:30 +0000

    Deploying from  phrase/openapi@d1c3ff09

--- src/main/java/com/phrase/client/model/TagWithStats1.java
+++ src/main/java/com/phrase/client/model/TagWithStats1.java
@@ -29,7 +29,7 @@ import java.util.List;
 /**
  * TagWithStats1
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2020-07-07T12:03:36.530Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2020-07-07T12:13:26.459Z[Etc/UTC]")
 public class TagWithStats1 {
   public static final String SERIALIZED_NAME_STATISTICS = "statistics";
   @SerializedName(SERIALIZED_NAME_STATISTICS)
