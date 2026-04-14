9bd41af44e38d3ab646b2d0ec4e11e95d9b63b11
Phrase <support@phrase.com>
2026-04-14 12:51:00 +0000

    Deploying from  phrase/openapi@6f8863b8

--- src/main/java/com/phrase/client/model/AbstractOpenApiSchema.java
+++ src/main/java/com/phrase/client/model/AbstractOpenApiSchema.java
@@ -22,7 +22,7 @@ import java.util.Map;
 /**
  * Abstract class for oneOf,anyOf schemas defined in OpenAPI spec
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-04-14T12:50:27.370230044Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-04-14T12:50:51.760681289Z[Etc/UTC]")
 public abstract class AbstractOpenApiSchema {
 
     // store the actual instance of the schema/object
--- src/main/java/com/phrase/client/model/OrganizationJobTemplateLocalesCreateParameters.java
+++ src/main/java/com/phrase/client/model/OrganizationJobTemplateLocalesCreateParameters.java
@@ -28,7 +28,7 @@ import io.swagger.annotations.ApiModelProperty;
 /**
  * OrganizationJobTemplateLocalesCreateParameters
  */
-@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-04-14T12:50:27.370230044Z[Etc/UTC]")
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", date = "2026-04-14T12:50:51.760681289Z[Etc/UTC]")
 public class OrganizationJobTemplateLocalesCreateParameters {
   public static final String SERIALIZED_NAME_LOCALE_NAME = "locale_name";
   @SerializedName(SERIALIZED_NAME_LOCALE_NAME)
